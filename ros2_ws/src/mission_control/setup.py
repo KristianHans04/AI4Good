345892d9a7366cba8429b134d18443287512d74f
KristianHans04 <theatwoodvlogers@gmail.com>
2026-01-04 13:35:31 +0300

    Add initial implementation of mission_control package with package.xml and setup.py

---
+++ ros2_ws/src/mission_control/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'mission_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/mission.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='robot@example.com',
+    description='Mission control with state machine for match logic',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mission_controller = mission_control.mission_controller:main',
+        ],
+    },
+)
